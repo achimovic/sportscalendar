f660cca5d95ebf4cc8f124da873cccecddbf6239
asedelmaier <achim.sedelmaier@gmail.com>
2020-08-24 11:29:49 +0200

    #CHANGE layout navigation bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,16 @@ require('../scss/app.scss');
 const $ = require('jquery');
 global.$ = global.jQuery = $;
 
+// bootstrap
+require('bootstrap/js/dist/collapse');
+require('bootstrap/js/dist/modal');
+
+// icons
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+library.add({ faSearch })
+dom.watch()
+
+global.bootbox = require('bootbox')
 require ('./custom')
